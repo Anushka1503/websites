61febc854574f0da26022542232b7b2074f6cc74
Anushka1503 <anushkaS.0315@gmail.com>
2022-04-06 23:58:09 +0530

    updated

--- spotify/pics/script.js
+++ spotify/pics/script.js
@@ -2,22 +2,28 @@ console.log("Welcome to Spotify");
 
 //initialize the variables
 let songIndex=0;
-let audioElement= new Audio('/pics/Tera Hone Laga Hoon.mp3');
+let audioElement= new Audio('/songs/Tera Hone Laga Hoon.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar=document.getElementById('myProgressBar')
 let gif=document.getElementById('gif')
-
+let songItems=Array.from(document.getElementsByClassName('songItem'));
 let songs=[
-    // {songName:'Salam-e-Ishq',filePath:'songs/1.mp3',coverPath:'covers/1.jpg'}
-    // {songName:'Salam-e-Ishq',filePath:'songs/1.mp3',coverPath:'covers/1.jpg'}
-    // {songName:'Salam-e-Ishq',filePath:'songs/2.mp3',coverPath:'covers/1.jpg'}
-    // {songName:'Salam-e-Ishq',filePath:'songs/3.mp3',coverPath:'covers/1.jpg'}
-    // {songName:'Salam-e-Ishq',filePath:'songs/4.mp3',coverPath:'covers/1.jpg'}
-    // {songName:'Salam-e-Ishq',filePath:'songs/5.mp3',coverPath:'covers/1.jpg'}
-    // {songName:'Salam-e-Ishq',filePath:'songs/6.mp3',coverPath:'covers/1.jpg'}
+    {songName:'Koi Mil Gaya - Kuch Kuch Hota Hai',filePath:'/songs/Koi Mil Gaya.mp3',coverPath:'/covers/kuch kuch hota hai.jpg'},
+    {songName:'Kaho Naa Pyaar hai - Kaho Naa Pyaar hai',filePath:'/songs/Kaho Naa Pyar Hai.mp3',coverPath:'/covers/knph1.jpg'},
+    {songName:'London thumakda - Queen',filePath:'/songs/London Thumakda.mp3',coverPath:'/covers/queen.jpg'},
+    {songName:'Main Rang Sharbaton Ka - Phata Poster Nikla Hero ',filePath:'/songs/Main Rang Sharbaton.mp3',coverPath:'/covers/ppnh1.jpg'},
+    {songName:'Maahi ve - Kal Ho Na Ho',filePath:'/songs/Maahi ve.mp3',coverPath:'/covers/khnh.jpg'},
+    {songName:'Tum Se Hi - Jab We Met',filePath:'/songs/Tum Se Hi.mp3',coverPath:'/covers/jab we met.jpg'},
+    {songName:'Tera Hone Laga Hoon - Ajab prem ki Gajab Kahani',filePath:'/songs/Tera Hone Laga Hoon.mp3',coverPath:'/covers/apgk.jpg'},
 
 ]
 
+songItems.forEach((element,i)=>{
+    console.log(element,i);
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+
+})
 
 // audioElement.play();
 
